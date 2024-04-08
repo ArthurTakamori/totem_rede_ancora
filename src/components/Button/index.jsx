0f3cb3a2f0db7213c702a73ed1b16f3e2ac6fd0c
ArthurTakamori <arthurguilhermes@gmail.com>
2024-04-07 22:49:48 -0300

    feat: Button Component
    Addition of General Button Component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Button() {
+
+  const navegar = useNavigate();
+  const navegarParaPagina =()=>{
+    navegar('/#')
+  }
+
   return (
     <>
-      
+    <button onClick={()=>navegarParaPagina()}>Botão</button>
     </>
   );
 }
