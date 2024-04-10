298da82a23c308533ebdd8aa12fb45ebc4f9aa4f
MarianaTThainara <marianathainara@yahoo.com>
2024-04-09 22:55:26 -0300

    fix: button

--- src/pages/splash/index.jsx
+++ src/pages/splash/index.jsx
@@ -1,6 +1,5 @@
 import Splash from '../../assets/img/splash.png';
 import { Link } from "react-router-dom";
-import Button from '../../components/Button';
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <Link to={'/identify'}>
         <img src={Splash} alt="Imagem principal"/>
       </Link>
-      <Button/>
     </>
   );
 }
