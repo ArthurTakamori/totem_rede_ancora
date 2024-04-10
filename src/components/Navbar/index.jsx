fd2b9195a78027ece1782055be74d1d23b69c6da
MarianaTThainara <marianathainara@yahoo.com>
2024-04-09 22:54:46 -0300

    feat: navbar animation

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,29 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
+
+  const navLinks = [
+    { 
+      to: "/home", 
+      icon_able: "mgc_home_3_fill",
+      icon_disable: "mgc_home_3_line" 
+    },
+    { 
+      to: "/search", 
+      icon_able: "mgc_search_2_line",
+      icon_disable: "mgc_search_2_line"
+    },
+    { 
+      to: "/search", 
+      icon_able: "mgc_car_door_line", 
+      icon_disable: "mgc_car_door_line" 
+    },
+    { 
+      to: "/profile", 
+      icon_able: "mgc_user_3_fill",
+      icon_disable: "mgc_user_3_line" 
+    }
+  ];
+
   return (
-    <>
-      <div>
-        <div className="row">
+    <div className="fixed-bottom p-4" style={{
+      height: '135px'
+    }}>
+      <div className="row">
 
-          <Link to="/home" className="col d-flex justify-content-center">
-            <span className="mgc_home_3_fill fs-1 rounded-circle border border-4 border-primary p-4"></span>
+        {navLinks.map((link, index) => (
+          
+          <Link key={index} to={link.to} className="col d-flex justify-content-center position-relative">
+
+            <div className={`rounded-circle p-3 border border-4 d-flex justify-content-center ${location.pathname === link.to ? 'active-link border-primary' : ''}`}>
+
+              <span className={
+                `fs-3 
+                ${location.pathname === link.to ? 
+                    `active-icon ${link.icon_able}` : 
+                    `inactive-icon ${link.icon_disable}`
+                }`}></span>
+
+            </div>
+
+            <span className={`${location.pathname === link.to ? 'd-block' : 'd-none'} position-absolute rounded-circle bg-primary p-1`} style={{width: '.775rem', height: '.775rem', bottom: '-20px'}}></span>
+
           </Link>
 
-          <Link to="/search" className="col d-flex justify-content-center">
-            <span className="mgc_search_2_line fs-1 rounded-circle border border-4 border-primary p-4"></span>
-          </Link>
+        ))}
 
-          <Link to="/search" className="col d-flex justify-content-center">
-            <span className="mgc_car_door_line fs-1 rounded-circle border border-4 border-primary p-4"></span>
-          </Link>
-
-          <Link to="/profile" className="col d-flex justify-content-center">
-            <span className="mgc_user_3_line fs-1 rounded-circle border border-4 border-primary p-4"></span>
-          </Link>
-
-        </div>
       </div>
-    </>
+    </div>
   );
 }
