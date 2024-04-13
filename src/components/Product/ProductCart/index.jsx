6ad8776caa5343b3808109d64ee2c254caf9160f
ArthurTakamori <arthurguilhermes@gmail.com>
2024-04-13 16:16:17 -0300

    Update index.jsx

--- src/components/Product/ProductCart/index.jsx
+++ src/components/Product/ProductCart/index.jsx
@@ -9,26 +9,22 @@ export default function Cart(){
     
     return (
         <>
-            <div className='d-flex align-items-center justify-content-between m-4 border border-dark '>
-                <div className="d-flex align-items-left">
-                    <img src={Amortecedor} alt="Imagem de Amortecedor" className='p-5 '/>
-                </div>
-                <div className='d-flex container align-items-center justify-content-between m-4'>
-                    <div className="border border-dark">
-                        <div className=''>
+            <div className='container border border-dark '>
+                <div className="d-flex row justify-content-center">
+                    <div className='d-flex col-md-4 justify-content-center'>
+                        <img src={Amortecedor} alt="Imagem de Amortecedor" className='justify-content-center'/>
+                    </div>
+                    <div className='col-md-8'>
+                        <div className='col-md-12'>
                             <h1>AMORTECEDOR</h1>
                             <p>MODELO ORIGINAL</p>
-                            <img src={Remover} alt="Imagem de Amortecedor" className='p-0 '/>
+                            <img src={Remover} alt="Imagem de Amortecedor" className='d-flex align-items-right'/>
                         </div>
-                    
-                    <div className="border border-dark">    
-                        <div className=''>
-                            <h2>R$244,50</h2>
+                        <div className=' col-md-12 '>
+                            <h2 className='pt-5'>R$244,50</h2>
                         </div>
                     </div>    
                 </div>
-            </div>
-
             </div>
         </>
     )
