2a29bc73f8ac5bf40002bf7896cf45d7cf5776b8
ArthurTakamori <arthurguilhermes@gmail.com>
2024-04-13 19:01:13 -0300

    feat: build cart product component v0.3

--- src/components/Product/ProductCart/index.jsx
+++ src/components/Product/ProductCart/index.jsx
@@ -9,21 +9,23 @@ export default function Cart(){
     
     return (
         <>
-            <div className='container-md border border-dark  '>
+            <div className='container-md border border-dark p-3 border-box-reset'>
                 <div className="d-flex row justify-content-center">
                     <div className='d-flex col-md-4 justify-content-center' style={{padding:"0px", margin:"0px"}}>
                         <img src={Amortecedor} alt="Imagem de Amortecedor" className='justify-content-center p-3' style={{height:"15rem"}}/>
                     </div>
                     <div className='col-md-8' style={{padding:"0px"}}>
-                        <div className='d-flex col-md-12 align-content-between  p-3 row' style={{height:"8rem"}}> 
-                            <h1 className='fs-3 p-0'>AMORTECEDOR</h1>
-                            <p className='fs-5 p-0 '>MODELO ORIGINAL</p>
-                            <div className='d-flex justify-content-end'>
-                                <img src={Remover} alt="Imagem de Amortecedor" className='align-items-right' style={{height:"3rem"}}/>
-                            </div>
+                        <div className='d-flex col-md-12 align-content-between p-3' style={{height:"2rem", padding:"0px"}}> 
+                            <h1 className='fs-1' style={{height:"2rem"}} >AMORTECEDOR</h1>
+                            <img src={Remover} alt="Imagem de Amortecedor" className='ms-auto' style={{height:"3rem"}}/>
                         </div>    
-                        <div className='d-flex col-md-12 p-3 align-content-between flex-wrap row' style={{height: "8rem"}}>
-                            <h2 className='pt-5'>R$244,50</h2>
+                            
+                        <div className='d-flex col-md-12 align-content-between p-3'>
+                            <p className='fs-4 p-0 '>MODELO ORIGINAL</p>
+                        </div>
+                            
+                        <div className='d-flex col-md-12 p-3 align-content-between flex-wrap' style={{height: "8rem", padding:"0px"}}>
+                            <h2 className='pt-5 p-0 '>R$244,50</h2>
                         </div>
                     </div>    
                 </div>
