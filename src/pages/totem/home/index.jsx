f7a2abd0bd71f5229f1e37dd1a454c548d3db3e6
MarianaTThainara <marianathainara@yahoo.com>
2024-05-09 20:38:21 -0300

    feat: banner home

--- src/pages/totem/home/index.jsx
+++ src/pages/totem/home/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Title from "@/components/Title";
 import { useEffect } from "react";
+import BannerImage from '@/assets/img/banner.png'
 
 export default function HomeTotem({ setSearchTerm, automakers }) {
   const handleAutomaker = (car) => {
@@ -21,21 +22,39 @@ export default function HomeTotem({ setSearchTerm, automakers }) {
 
   return (
     <>
-      <Title page={"Montadoras"} />
+
+      
 
       <div
         className="overflow-y-auto px-5"
         style={{
-          height: "calc(100% - 12rem)",
+          height: "calc(100% - 4rem)",
         }}
       >
-        <div className="row g-3">
+        <div className="rounded-2 mt-4 bg-white border-0 overflow-hidden position-relative" style={{
+          height: '32%',
+          cursor: 'pointer'
+        }}>
+          <img src={BannerImage} alt="Logo Rede Ancora" className="position-absolute top-0 h-100 w-100"/>
+
+        </div>
+        <a href="https://jornaldocarro.estadao.com.br/carros/nova-lei-das-placas-entra-em-vigor-e-gera-fake-news-na-inernet/" target="_blank" className="fs-6 fw-medium opacity-50">
+          Crédito imagem placa do carro: Jornal do carro
+        </a>
+
+        <div className="mt-4">
+          <Title page={"Montadoras disponíveis"} />
+        </div>
+
+        <div className="row g-3 px-4" style={{
+          marginBottom: '5rem'
+        }}>
           {automakers.map((car, index) => (
             <div className="col col-sm-6 col-md-4 col-lg-3" key={index}>
               <Link
                 to="/totem/dashboard/search"
                 className="w-full card-category rounded-1 d-flex flex-column justify-content-around align-items-center p-4 fw-medium fs-5 text-center"
-                style={{ minHeight: "250px" }}
+                style={{ minHeight: "220px" }}
                 onClick={() => handleAutomaker(car)}
               >
                 <span
