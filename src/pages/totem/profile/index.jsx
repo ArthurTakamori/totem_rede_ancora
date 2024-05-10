038f42120b18c29141639a46846d69b27ffcdc98
ArthurTakamori <arthurguilhermes@gmail.com>
2024-05-10 15:12:45 -0300

    feat: profile update responsiveness v1.5

--- src/pages/totem/profile/index.jsx
+++ src/pages/totem/profile/index.jsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
-import { hideCPF, maskCpf } from "@/utils/maskCpf";
 import Title from "@/components/Title";
-import { Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup"
 
@@ -16,17 +14,6 @@ const schema = yup.object({
   // license_plate: yup.string().length(7, 'O número da placa deve conter 7 caracteres').required('O número da placa do veículo é obrigatório')
 })
 
-// {
-//   name: "Carlos Oliveira",
-//   email: "carlos.oliveira@example.com",
-//   phone: "(11) 87654-3210",
-//   cpf: "876.543.210-99",
-//   cars: [
-//     {
-//       license_plate: "JKL-3456",
-//     },
-//   ],
-// }
 
 export default function Signing(){
   
@@ -42,75 +29,73 @@ function userAdd(user){
   return(
     <>
       <Title page={"Minha Conta"}/>
-
-      <form onSubmit={handleSubmit(userAdd)} autoComplete="off" className="px-4">
-        <fieldset className="d-flex flex-column gap-3">
-        <div>
-            <label className="required">Nome completo</label>
-              <Input type="text" 
-              required={true}
-              {...register("name")}
-              />
-              <span>{errors.name?.message}</span>
-          </div>
-          <div className="col-12 col-md-12 p-0">
-            <div className='d-flex align-items-center justify-content-between w-100'>
-              <label className="required">CPF</label>
-              <button
-                type="button"
-                onClick={() => setShowCpf(!showCpf)}
-                className='rounded-circle d-flex align-items-center justify-content-center'
-                style={{
-                  display:"flex",
-                }}
-                >
-                <span
-                  className={`${
-                    showCpf ? "mgc_eye_2_line" : "mgc_eye_close_line"
-                  } fs-1`}
+      <div className="col-12 col-md-12 bg-white">
+        <form onSubmit={handleSubmit(userAdd)} autoComplete="off" className="px-4 py-4">
+          <fieldset className="d-flex flex-column gap-2">
+          <div>
+              <label className="required">Nome completo</label>
+                <Input type="text"
+                required={true}
+                {...register("name")}
                 />
-                </button>
-              </div>
-              <Input type={showCpf === true ? 'text' : 'password'}
-              required={true} 
-              {...register("cpf")}
-              />
+                <span>{errors.name?.message}</span>
             </div>
-            <span>{errors.cpf?.message}</span>
-          
-          <div>
-            <label className="required">Telefone</label>
-              <Input type="number"
-              required={true}
-              maxLength="11" 
-              {...register("phone")}
-              />
-              <span>{errors.phone?.message}</span>
-          </div>
+            
+            <div className="col-12 col-md-12">
+              <div className='d-flex align-items-center justify-content-between w-100'>
+                <label className="required">CPF</label>
+                <button
+                  type="button"
+                  onClick={() => setShowCpf(!showCpf)}
 
-          <div>
-            <label className="required">E-mail</label>
-              <Input type="text"
-              required={true} 
-              {...register("email")}
-              />
-              <span>{errors.email?.message}</span>
-          </div>
+                  >
+                  <span
+                    className={`${
+                      showCpf ? "mgc_eye_2_line" : "mgc_eye_close_line"
+                    } fs-1`}
+                  />
+                  </button>
+                </div>
+                <Input type={showCpf === true ? 'text' : 'password'}
+                required={true} 
+                {...register("cpf")}
+                />
+              </div>
+              <span>{errors.cpf?.message}</span>
+            
+            <div>
+              <label className="required">Telefone</label>
+                <Input type="number"
+                required={true}
+                {...register("phone")}
+                />
+                <span>{errors.phone?.message}</span>
+            </div>
 
-          <div>
-            <label className="notRequired">Placa do Carro</label>
-              <Input type="text"
-              required={false} 
-              {...register("license_plate")}
-              />
-              <span>{errors.license_plate?.message}</span>
-          </div>
+            <div>
+              <label className="required">E-mail</label>
+                <Input type="text"
+                required={true} 
+                {...register("email")}
+                />
+                <span>{errors.email?.message}</span>
+            </div>
 
-          <div className="d-flex w-100 justify-content-end mt-4">
-            <Button name={"Atualizar dados cadastrais"} type="submit"/>
-          </div>
-        </fieldset>
-      </form>
+            <div>
+              <label className="notRequired">Placa do Carro</label>
+                <Input type="text"
+                required={false} 
+                {...register("license_plate")}
+                />
+                <span>{errors.license_plate?.message}</span>
+            </div>
+
+            <div className="d-flex w-100 justify-content-end mt-4">
+              <Button name={"Atualizar dados cadastrais"} type="submit"/>
+            </div>
+          </fieldset>
+        </form>
+      </div>
     </>
   )
 }
