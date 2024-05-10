e204dce08e11bfa7b8ea1cb2e5bd625ac6012251
ArthurTakamori <arthurguilhermes@gmail.com>
2024-05-10 00:27:17 -0300

    feat: hide cpf button

--- src/pages/totem/profile/index.jsx
+++ src/pages/totem/profile/index.jsx
@@ -5,15 +5,34 @@ import Button from "@/components/Button";
 import { hideCPF, maskCpf } from "@/utils/maskCpf";
 import Title from "@/components/Title";
 import { Controller } from "react-hook-form";
-import { getUser } from "@/state/userState";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup"
 
+const schema = yup.object({
+  name: yup.string().required('O nome é obrigatório'),
+  cpf: yup.string().min(11, 'O CPF deve ter no mínimo 11 caracteres').required('O CPF é obrigatório'),
+  phone: yup.string().min(10, 'O telefone deve conter no mínimo 10 caracteres incluindo o DDD ').required('O telefone é obrigatório'),
+  email: yup.string().email('Este formato de e-mail é inválido').required('O e-mail é obrigatório'),
+  license_plate: yup.string().length(7, 'O número da placa deve conter 7 caracteres').required('O número da placa do veículo é obrigatório')
+})
+
+// {
+//   name: "Carlos Oliveira",
+//   email: "carlos.oliveira@example.com",
+//   phone: "(11) 87654-3210",
+//   cpf: "876.543.210-99",
+//   cars: [
+//     {
+//       license_plate: "JKL-3456",
+//     },
+//   ],
+// }
 
 export default function Signing(){
   
 const [userList, setUserList] = useState([])
-const {register, handleSubmit, formState:{errors}, setValue, setFocus} = useForm({})
+const [showCpf, setShowCpf] = useState(true);
+const {register, handleSubmit, formState:{errors}} = useForm({resolver:yupResolver(schema)})
 
 function userAdd(user){
   setUserList([...userList, user])
@@ -23,28 +42,57 @@ function userAdd(user){
   return(
     <>
       <Title page={"Minha Conta"}/>
-
       <form onSubmit={handleSubmit(userAdd)} autoComplete="off" className="px-4">
-        <fieldset>
+        <fieldset className="d-flex flex-column gap-3">
           <label>Nome completo
-            <input type="text" 
-            {...register("nome")} 
-          />
+            <Input type="text" 
+            {...register("name")} 
+            />
+            <span>{errors.nome?.message}</span>
           </label>
+          
           <label>CPF
-            <input type="text" 
+            <Input type={showCpf === true ? 'text' : 'password'} 
             {...register("cpf")}
-          />
+            />
+            <span>{errors.cpf?.message}</span>
           </label>
+          
+          <div className="position-relative">
+            <button
+              type="button"
+              onClick={() => setShowCpf(!showCpf)}
+              className="position-absolute"
+              style={{
+                top: "-102px",
+                right: "15px",
+              }}
+            >
+              <span
+                className={`${
+                  showCpf ? "mgc_eye_2_line" : "mgc_eye_close_line"
+                } fs-1`}
+              />
+            </button>
+          </div>
+          
           <label>Telefone
-            <input type="text" 
-            {...register("telefone")}
-          />
+            <Input type="text" 
+            {...register("phone")}
+            />
+            <span>{errors.phone?.message}</span>
           </label>
           <label>E-mail
-            <input type="text" 
+            <Input type="text" 
             {...register("email")}
-          />
+            />
+            <span>{errors.email?.message}</span>
+          </label>
+          <label>Placa do Carro
+            <Input type="text" 
+            {...register("license_plate")}
+            />
+            <span>{errors.license_plate?.message}</span>
           </label>
 
           <div className="d-flex w-100 justify-content-end mt-4">
@@ -53,9 +101,5 @@ function userAdd(user){
         </fieldset>
       </form>
     </>
-
-
-  
-  
   )
 }
